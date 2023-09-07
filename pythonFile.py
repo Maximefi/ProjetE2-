8b6aa52e0ed61b0f7c9df2fd37d0a611df7fdbe7
Maximefi <144149168+Maximefi@users.noreply.github.com>
2023-09-07 14:09:43 +0200

    Update pythonFile.py

--- pythonFile.py
+++ pythonFile.py
@@ -1,4 +1,10 @@
-du code python
+le détecteur d'obstacle:
+recherche du détecteur à utiliser
+rédaction d'un rapport avec les caractéristiques du capteur choisit, les raisons du choix et la pertinence de celui-ci 
+recherche du bon avertisseur (sonore, notification, vibration ....) 
+rédaction d'un rapport avec les caractéristiques du capteur choisit, les raisons du choix et la pertinence de celui-ci 
+rédaction du code et optimisation de celui-ci 
+premier test et débugage 
+réfléchir sur la disposition de l'ensemble sur la valise  (hauteur, largeur...)
 
-imaginons que le fichier python est modifié sur internet par quelqu'un. donc je n'ai plus la bonne version :/
-YOUPY !
+
